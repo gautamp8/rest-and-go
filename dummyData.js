532d73bc4b0cfb8fb1471568b2db2154d587bbc8
Gautam Prajapati <gautamprajapati06@gmail.com>
2017-10-27 16:59:55 +0530

    Update dummy data

--- dummyData.js
+++ dummyData.js
@@ -1,5 +1,5 @@
 use store;
-var bulk = db.albums.initializeUnorderedBulkOp();
+var bulk = db.products.initializeUnorderedBulkOp();
 bulk.insert(   { title: "Apple iMac Pro", image: "http:://example.com/p1.jpg", price: 5000, rating: 4, id: 1 });
 bulk.insert(   { title: "Google Pixel 2", image: "http:://example.com/p2.jpg", price: 2000, rating: 5, id: 2 });
 bulk.insert(   { title: "Apple iPhone X", image: "http:://example.com/p3.jpg", price: 3000, rating: 5, id: 3 });
